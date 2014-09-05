c025a2ee7ce63340f8586242c16ced5a1db90438
Paul Kim <paulhyunkim@gmail.com>
2014-09-05 09:31:47 -0700

    added activemodelserializer initializer file that removes top level wrapper {} so an array gets returns instead of an object

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,7 +1,10 @@
 class ScoresController < ApplicationController
+	respond_to :json
 
 	# retrieve list of scores
 	def index
+		@scores = Score.all
+		respond_with @scores
 	end
 
 	# save user's scores
