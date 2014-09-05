4c6e4813e4d239d47665c8b6fd210eb5db3680db
Paul Kim <paulhyunkim@gmail.com>
2014-09-05 09:38:31 -0700

    wrote cards#create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,6 +7,20 @@ class CardsController < ApplicationController
 	end
 
 	def create
+		@card = Card.new(card_params)
+
+		if @card.save
+			respond_to do |format|
+			format.html { }
+      format.json { render json: @card, status: :created }
+    end
+  else
+    respond_to do |format|
+      format.html { }
+      format.json { render json: @card.errors, status: :unprocessable_entity }
+    end
+  end
+
 	end
 
 	def edit
@@ -14,5 +28,10 @@ class CardsController < ApplicationController
 
 	def destroy
 	end
+
+	protected
+
+	def card_params
+		params.require(:card).permit(:question, :answer)
 
 end
