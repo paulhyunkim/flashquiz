5119ddaa0d2163ff46d36bcb4fd5757d315078e9
Paul Kim <paulhyunkim@gmail.com>
2014-09-05 09:21:20 -0700

    created angular module and controller and hooked up to view to test data binding

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,18 @@
+var app = angular.module('FlashQuizApp', ['ngResource']);
+
+app.config(['$httpProvider', function($httpProvider) {
+	var authToken = angular.element('meta[name=\"csrf-token\"]').attr('content');
+	var defaults = $httpProvider.defaults.headers;
+
+	defaults.common['X-CSRF-TOKEN'] = authToken;
+	defaults.patch = defaults.patch || {};
+	defaults.patch['Content-Type'] = 'application/json';
+	defaults.common['Accept'] = 'application/json';
+}]);
+
+app.controller('MainController', ['$scope', '$timeout', '$http',
+	function($scope, $timeout, $http) {
+
+		$scope.name = "Paul";
+
+	}]);
